e822b0d613cc57d8830a2f970f6508399c6a584c
Dhruv <dhruvchaturvediiitb@gmail.com>
2025-09-29 18:50:03 +0530

    completed the value retrieval

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ var _a, _b;
 let insentropic_val = ['Mach Number (M)', 'Mach Angle θ', 'P-M angle φ', 'Pressure Ratio (P/P0)', 'Temperature Ratio (T/T0)', 'Density Ratio (ρ/ρ0)', 'Area Ratio (A/A*)',];
 let normal_shock_val = ['Mach Number (M1)', 'Mach Number (M2)', 'Total Pressure Ratio (P02/P01)', 'Pressure Ratio (P1/P02)', 'Pressure Ratio (P2/P1)', 'Temperature Ratio (T2/T1)', 'Density Ratio (ρ2/ρ1)'];
 let oblique_shock_val = ['Mach Number (M2)', 'Turn Angle (θ)', 'Wave Angle (β)', 'Pressure Ratio (P2/P1)', 'Density Ratio (ρ2/ρ1)', 'Temperature Ratio (T2/T1)', 'Total Pressure Ratio (P02/P01)', 'Normal Mach Number (M2)'];
+// To Do
+// 1. Add Error Handling
+// 2. Add Output Table
+// 3. Add CSS
+// 4. Add Comments and Documentation
+// 5. Value range check for inputs
 // Giving two Inputs for Oblique Shocks
 (_a = document.getElementById('relation')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', () => {
     const relation = document.getElementById('relation').value;
@@ -68,6 +74,80 @@ let oblique_shock_val = ['Mach Number (M2)', 'Turn Angle (θ)', 'Wave Angle (β)
 });
 const calculateGasTables = (gamma, relation, inputVal) => {
     console.log(`γ: ${gamma}, Relation: ${relation}, Input Value: ${inputVal}`);
+    const relationInstance = new relationClass(gamma, relation, inputVal);
+    const result = relationInstance.calculate();
+    console.log(result);
 };
+class relationClass {
+    constructor(gamma, relation, inputVal) {
+        this.gamma = gamma;
+        this.relation = relation;
+        this.inputVal = inputVal;
+    }
+    calculate() {
+        if (this.relation === 'isentropic') {
+            return this.isentropicCalc();
+        }
+        else if (this.relation === 'normal') {
+            return this.normalShockCalc();
+        }
+        else if (this.relation === 'oblique') {
+            return this.obliqueShockCalc();
+        }
+        else {
+            throw new Error('Invalid relation type');
+        }
+    }
+    isentropicCalc() {
+        const inputParam = this.inputVal[0];
+        const inputValue = parseFloat(this.inputVal[1] || '0');
+        if (inputParam !== 'mach_number') {
+        }
+        else {
+            return this.isentropicCalc_mach(this.gamma, inputValue);
+        }
+    }
+    isentropicCalc_mach(gamma, M) {
+        let theta, phi, P_P0, T_T0, rho_rho0, A_Astar;
+        // Critical Values
+        let Pc, // p/p* 
+        Tc, // T/T*
+        rhoc; // rho/rho*
+        P_P0 = Math.pow(1 + (this.gamma - 1) / 2 * M * M, -this.gamma / (this.gamma - 1));
+        T_T0 = Math.pow(1 + (this.gamma - 1) / 2 * M * M, -1);
+        rho_rho0 = Math.pow(1 + (this.gamma - 1) / 2 * M * M, -1 / (this.gamma - 1));
+        A_Astar = (1 / M) * Math.pow((2 / (this.gamma + 1)) * (1 + (this.gamma - 1) / 2 * M * M), (this.gamma + 1) / (2 * (this.gamma - 1)));
+        theta = Math.asin(1 / M) * (180 / Math.PI); // in degrees
+        phi = (Math.sqrt((this.gamma + 1) / (this.gamma - 1)) * Math.atan(Math.sqrt((this.gamma - 1) / (this.gamma + 1) * (M * M - 1))) - Math.atan(Math.sqrt(M * M - 1))) * (180 / Math.PI); // in degrees
+        // Critical Values
+        let gamma_critical = (this.gamma + 1) / 2; // It is constant to help in calculations not actually gamma critical
+        Pc = Math.pow(gamma_critical, this.gamma / (this.gamma - 1)) * P_P0;
+        Tc = T_T0 * gamma_critical;
+        rhoc = Math.pow(gamma_critical, 1 / (this.gamma - 1)) * rho_rho0;
+        return {
+            M,
+            theta,
+            phi,
+            P_P0,
+            T_T0,
+            rho_rho0,
+            A_Astar,
+            Pc,
+            Tc,
+            rhoc
+        };
+    }
+    normalShockCalc() {
+        const inputParam = this.inputVal[0];
+        const inputValue = parseFloat(this.inputVal[1] || '0');
+        let M1, M2, P02_P01, P1_P02, P2_P1, T2_T1, rho2_rho1;
+    }
+    obliqueShockCalc() {
+        const M1 = parseFloat(this.inputVal[0] || '0');
+        const inputParam = this.inputVal[1];
+        const inputValue = parseFloat(this.inputVal[2] || '0');
+        let M2, theta, beta, P2_P1, rho2_rho1, T2_T1, P02_P01, M1n;
+    }
+}
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ let insentropic_val:string[] = ['Mach Number (M)','Mach Angle θ','P-M angle φ'
 let normal_shock_val:string[] = ['Mach Number (M1)','Mach Number (M2)', 'Total Pressure Ratio (P02/P01)','Pressure Ratio (P1/P02)','Pressure Ratio (P2/P1)', 'Temperature Ratio (T2/T1)', 'Density Ratio (ρ2/ρ1)'];
 let oblique_shock_val:string[] = ['Mach Number (M2)', 'Turn Angle (θ)', 'Wave Angle (β)', 'Pressure Ratio (P2/P1)', 'Density Ratio (ρ2/ρ1)', 'Temperature Ratio (T2/T1)', 'Total Pressure Ratio (P02/P01)', 'Normal Mach Number (M2)'];
 
+// To Do
+// 1. Add Error Handling
+// 2. Add Output Table
+// 3. Add CSS
+// 4. Add Comments and Documentation
+// 5. Value range check for inputs
 
 // Giving two Inputs for Oblique Shocks
 
@@ -83,6 +89,101 @@ document.getElementById('calculateBtn')?.addEventListener('click', () => {
 const calculateGasTables = (gamma: number, relation: string, inputVal: string[]) => {
     
     console.log(`γ: ${gamma}, Relation: ${relation}, Input Value: ${inputVal}`);
+    const relationInstance = new relationClass(gamma, relation, inputVal);
+    const result = relationInstance.calculate();
+    console.log(result);
 
+
+}
+
+class relationClass {
+    gamma: number;
+    relation: string;
+    inputVal: string[];
+    constructor(gamma: number, relation: string, inputVal: string[]) {
+        this.gamma = gamma;
+        this.relation = relation;
+        this.inputVal = inputVal;
+    }
+
+    calculate() {
+        if (this.relation === 'isentropic') {
+            return this.isentropicCalc();
+        } else if (this.relation === 'normal') {
+            return this.normalShockCalc();
+        } else if (this.relation === 'oblique') {
+            return this.obliqueShockCalc();
+        } else {
+            throw new Error('Invalid relation type');
+        }
+        
+    }
+    private isentropicCalc() {
+        const inputParam = this.inputVal[0];
+        const inputValue = parseFloat(this.inputVal[1] || '0');
+
+
+
+        if (inputParam !== 'mach_number') {
+            
+            
+
+        }else{
+            return this.isentropicCalc_mach(this.gamma, inputValue);
+        }
+    }
+
+
+    private isentropicCalc_mach(gamma:number, M: number) {
+        let theta: number, phi: number, P_P0: number, T_T0: number, rho_rho0: number, A_Astar: number;
+        // Critical Values
+        let Pc:number, // p/p* 
+        Tc:number, // T/T*
+        rhoc:number; // rho/rho*
+       
+       
+        P_P0 = Math.pow(1 + (this.gamma - 1) / 2 * M * M, -this.gamma / (this.gamma - 1));
+        T_T0 = Math.pow(1 + (this.gamma - 1) / 2 * M * M, -1);
+        rho_rho0 = Math.pow(1 + (this.gamma - 1) / 2 * M * M, -1 / (this.gamma - 1));
+        A_Astar = (1 / M) * Math.pow((2 / (this.gamma + 1)) * (1 + (this.gamma - 1) / 2 * M * M), (this.gamma + 1) / (2 * (this.gamma - 1)));
+
+        theta = Math.asin(1 / M) * (180 / Math.PI); // in degrees
+        phi = (Math.sqrt((this.gamma+1)/(this.gamma-1)) * Math.atan(Math.sqrt((this.gamma-1)/(this.gamma+1)*(M*M-1))) - Math.atan(Math.sqrt(M*M-1))) * (180/Math.PI); // in degrees
+
+        // Critical Values
+        let gamma_critical: number = (this.gamma + 1) / 2; // It is constant to help in calculations not actually gamma critical
+        
+        Pc = Math.pow(gamma_critical, this.gamma / (this.gamma - 1)) * P_P0;
+        Tc = T_T0 * gamma_critical;
+        rhoc = Math.pow(gamma_critical, 1 / (this.gamma - 1))* rho_rho0;
+
+        return {
+            M,
+            theta,
+            phi,
+            P_P0,
+            T_T0,
+            rho_rho0,
+            A_Astar,
+            Pc,
+            Tc,
+            rhoc
+        };
+    
+    
+    }
+        
+    private normalShockCalc() {
+        const inputParam = this.inputVal[0];
+        const inputValue = parseFloat(this.inputVal[1] || '0');
+        let M1: number, M2: number, P02_P01: number, P1_P02: number, P2_P1: number, T2_T1: number, rho2_rho1: number;
+    }
+    private obliqueShockCalc() {
+        const M1 = parseFloat(this.inputVal[0] || '0');
+        const inputParam = this.inputVal[1];
+        const inputValue = parseFloat(this.inputVal[2] || '0');
+        let M2: number, theta: number, beta: number, P2_P1: number, rho2_rho1: number, T2_T1: number, P02_P01: number, M1n: number;
+    }
+ 
 }
 
